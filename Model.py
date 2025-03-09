38fb8bb3a658124caf4259ebaef32fd8c1d74b5a
kylanli <kylanyli20@gmail.com>
2025-03-09 12:01:02 -0400

    LSTM skeleton progress-forward pass complete

--- Model.py
+++ Model.py
@@ -4,5 +4,61 @@ class LSTM:
     def __init__(self, input_dim, hidden_dim):
         # initializing the dimensions
         self.input_dim = input_dim # input_dimension is defined by the number of features
-        self.hidden_dim = hidden_dim
+        self.hidden_dim = hidden_dim # Number of hidden neurons in model. Appropriate # --> balance of complexity
 
+        # Xavier Initialization for weights
+        # What is Xavier Initialization? The Xavier Initialization accounts for issues of exploding and shrinking
+        # gradients by normalizing variance accordingly. "Basically, an additional safety measure"
+
+        # NOTE: all weight and biases are defined as NumPy arrays
+
+        # Weights and biases for forget gate
+        self.W_f = np.random.randn(hidden_dim, input_dim + hidden_dim) * np.sqrt(1 / input_dim + hidden_dim)
+        self.b_f = np.zeros((hidden_dim, 1))
+
+        # Weights and biases for input gate
+        self.W_i = np.random.randn(hidden_dim, input_dim + hidden_dim) * np.sqrt(1 / input_dim + hidden_dim)
+        self.b_i = np.zeros((hidden_dim, 1))
+
+        # Weights and biases for cell-state gate
+        self.W_c = np.random.randn(hidden_dim, input_dim + hidden_dim) * np.sqrt(1 / input_dim + hidden_dim)
+        self.b_c = np.zeros((hidden_dim, 1))
+
+        # Weights and biases for output gate
+        self.W_o = np.random.randn(hidden_dim, input_dim + hidden_dim) * np.sqrt(1 / input_dim + hidden_dim)
+        self.b_o = np.zeros((hidden_dim, 1))
+
+    # activation functions
+    @staticmethod
+    def sigmoid(x):
+        return 1 / (1 + np.exp(-x))
+
+    @staticmethod
+    def tanh_derivative(x):
+        return 1 - np.tanh(x) ** 2
+
+    # forward pass method.
+    def forward(self, x):  # here, x is a 2D numpy array. For us, this would be the weather parameters at time t.
+        T = x.shape[0]  # T represents length of input
+        h, c = np.zeros((self.hidden_dim, 1))  # h (hidden state) and c (cell state) are initialized as empty vectors. Will be updated
+
+        self.cache = [] # stores intermediate values (that will be added later), required for back-propagation
+        h_seq = [] # stores hidden_states (short-term memory) as it is being updated.
+
+        for t in range(T):
+            xt = x[t].reshape(-1,1)  # for the number of parameters, reshape each parameter as a column vector
+            combined = np.vstack((h, xt))  # stacks hidden state and input into a combined "column vector"
+
+            fg = self.sigmoid(np.dot(self.W_f, combined) + self.b_f)  # calculations for forget gate
+            # not sure why activation functions is giving error
+            ig = self.sigmoid(np.dot(self.W_i, combined) + self.b_i)  # calculations for input gate.
+            cg = np.tanh(np.dot(self.W_c, combined) + self.b_c)  # calculations for a potential new cell
+            c = fg * c + cg * ig  # updating cell state value
+
+            og = self.sigmoid(np.dot(self.W_o, combined) + self.b_o)  # calculating for output gate
+            h = og * np.tanh(c)
+
+            self.cache.append((h, c, fg, ig, cg, combined))  # adding these updated values to the cache. Each tuple represents one time step.
+            h_seq.append(h)  # updating hidden state value
+
+        return np.array(h_seq)  # returns the vector h_seq for the next iteration as a nparray.
